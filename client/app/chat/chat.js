9f842d065ccf7710cec0ae9cf81026809ae58173
Sam Phillips <armyf35@gmail.com>
2016-11-28 13:28:30 -0500

    Added users array and handler for user login

--- client/app/chat/chat.js
+++ client/app/chat/chat.js
@@ -3,8 +3,14 @@ angular.module('socket-chat.chat', [
 ])
 
 .controller('ChatController', function ($scope, Socket) {
+  $scope.users = [];
+
   $scope.promptName = function() {
     $scope.name = prompt('What is your name?') || 'anonymous';
     Socket.login($scope.name);
   };
+
+  Socket.on('login', function(name) {
+    $scope.users.push(name);
+  });
 });
