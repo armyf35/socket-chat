f79098797d22fdd018edb7fc557cea0ce68ead46
Sam Phillips <armyf35@gmail.com>
2016-11-28 14:58:06 -0500

    Added api routes for recent messages and active users

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,15 @@
-module.exports = function (app, express) {
+module.exports = function (app, express, activeUsers, messages) {
+  app.get('/api/users/active', function(req, res) {
+    res.json(activeUsers);
+  });
+
+  app.get('/api/messages/recent', function(req, res) {
+    let tempMessages = messages.slice(-20);
+    tempMessages.reverse();
+
+    res.json(tempMessages);
+  });
+
   app.get('*', function(req, res) {
     res.send('<h1>Hello world</h1>');
   });
