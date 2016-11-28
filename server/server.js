427e779f63bab299151e09d90ff3d9c8432788dd
Sam Phillips <armyf35@gmail.com>
2016-11-28 14:57:48 -0500

    Added tracking of activeUsers and messages

--- server/server.js
+++ server/server.js
@@ -3,22 +3,27 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var activeUsers = [];
+var messages = [];
 
 require('./config/middleware.js')(app, express);
-require('./config/routes.js')(app, express);
+require('./config/routes.js')(app, express, activeUsers, messages);
 
 io.on('connection', function(socket) {
   socket.on('login', function(name) {
     socket.name = name;
-    socket.broadcast.emit('login', socket.name);
+    socket.broadcast.emit('login', name);
+    activeUsers.push(name);
   });
 
   socket.on('disconnect', function() {
     socket.broadcast.emit('logout', socket.name);
+    activeUsers.splice(activeUsers.indexOf(socket.name), 1);
   });
 
   socket.on('message', function(msg) {
     socket.broadcast.emit('message', msg);
+    messages.push(msg);
   });
 });
 
