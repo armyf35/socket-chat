342c5241ce6c3d4d8cc34ed4dc823609ad42f8eb
Sam Phillips <armyf35@gmail.com>
2016-11-28 12:22:34 -0500

    Added prompt for name when loading the page

--- client/app/chat/chat.js
+++ client/app/chat/chat.js
@@ -2,4 +2,8 @@ angular.module('socket-chat.chat', [])
 
 .controller('ChatController', function ($scope) {
   
+.controller('ChatController', function ($scope, Socket) {
+  $scope.promptName = function() {
+    $scope.name = prompt('What is your name?') || 'anonymous';
+  };
 });
