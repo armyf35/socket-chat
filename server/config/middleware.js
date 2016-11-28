2fe460a5614c24359f361c521374f619aacf01d3
Sam Phillips <armyf35@gmail.com>
2016-11-28 16:11:14 -0500

    Changed static route to node_modules

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,5 +1,5 @@
 module.exports = function (app, express) {
   app.use(express.static(__dirname + '/../../client'));
-  app.use('/lib', express.static(__dirname + '/../../node_modules'));
+  app.use('/node_modules', express.static(__dirname + '/../../node_modules'));
   app.use(express.static(__dirname + '/public'));
 };
