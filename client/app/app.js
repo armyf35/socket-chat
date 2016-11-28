bcb9288bd179dd3559992f626d83626f93bc825c
Sam Phillips <armyf35@gmail.com>
2016-11-28 11:34:04 -0500

    Added chat module

--- client/app/app.js
+++ client/app/app.js
@@ -1,9 +1,12 @@
 angular.module('socket-chat', [
+  'socket-chat.chat',
   'ngRoute'
 ])
 .config(function ($routeProvider) {
   $routeProvider
     .when('/home', {
+      templateUrl: 'app/chat/home.html',
+      controller: 'ChatController'
     })
     .otherwise({
       redirectTo: '/home'
---
+++ client/app/chat/chat.js
@@ -0,0 +1,5 @@
+angular.module('socket-chat.chat', [])
+
+.controller('ChatController', function ($scope) {
+  
+});
