dcffb817444a7c01570e098c3ee00ff9bd8627ed
Sam Phillips <armyf35@gmail.com>
2016-11-28 12:22:05 -0500

    Added sockets to server

--- server/server.js
+++ server/server.js
@@ -2,9 +2,14 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+});
 
 http.listen(process.env.PORT, function() {
   console.log(`listening on *:${process.env.PORT}`);
