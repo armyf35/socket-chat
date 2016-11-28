823a9bf891d73698a8e94f2e1262c9fe72768fd2
Sam Phillips <armyf35@gmail.com>
2016-11-28 11:15:12 -0500

    Added static middleware

---
+++ server/config/middleware.js
@@ -0,0 +1,4 @@
+module.exports = function (app, express) {
+  app.use(express.static(__dirname + '/../../client'));
+  app.use('/lib', express.static(__dirname + '/../../node_modules'));
+};
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
+require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
 http.listen(process.env.PORT, function() {
