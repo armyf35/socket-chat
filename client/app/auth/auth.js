c05b81c66a88291c8feb41b37abfe484f0159b17
Sam Phillips <armyf35@gmail.com>
2016-11-29 10:17:26 -0500

    Added signin/signup and logout to authcontroller

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,4 +1,29 @@
 angular.module('socket-chat.auth', [])
-.controller('AuthController', function($scope, Socket) {
+.controller('AuthController', function($scope, $window, $location, Auth, Socket) {
+  if ($location.$$path === '/logout') {
+    Auth.signout();
+  }
+  $scope.user = {};
 
+  $scope.signin = function () {
+    Auth.signin($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.socket-chat', token);
+        $location.path('/chats');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  $scope.signup = function () {
+    Auth.signup($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.socket-chat', token);
+        $location.path('/chats');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 });
