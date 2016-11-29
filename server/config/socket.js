63fefa929e3354510c1295a7614a0e15168ab9db
Sam Phillips <armyf35@gmail.com>
2016-11-29 15:12:27 -0500

    Removed connection count socket and message broadcast

--- server/config/socket.js
+++ server/config/socket.js
@@ -2,12 +2,8 @@ const User = require('../models/user');
 const Messages = require('../collections/messages');
 
 module.exports = function(io, activeUsers) {
-  var connectionCount = 0;
 
   io.on('connection', function(socket) {
-    connectionCount++;
-    socket.broadcast.emit('count', connectionCount);
-
     socket.on('login', function(name) {
       socket.name = name;
       socket.broadcast.emit('login', name);
@@ -20,28 +16,10 @@ module.exports = function(io, activeUsers) {
     });
 
     socket.on('disconnect', function() {
-      connectionCount--;
-      socket.broadcast.emit('count', connectionCount);
       if (socket.name) {
         socket.broadcast.emit('logout', socket.name);
         activeUsers.splice(activeUsers.indexOf(socket.name), 1);
       }
-    });
-
-    socket.on('message', function(msg) {
-      socket.broadcast.emit('message', msg);
-
-      new User({ username: msg.user }).fetch().then((found) => {
-        Messages.create({
-          text: msg.text,
-          user_id: found.id,
-          created_at: msg.created_at
-        });
-      });
-    });
-
-    socket.on('active', function() {
-      socket.emit('count', connectionCount);
     });
   });
 };
