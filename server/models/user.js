10613c25d2775483df3c0290476da8a676eb6197
Sam Phillips <armyf35@gmail.com>
2016-11-28 20:06:29 -0500

    Added user model

---
+++ server/models/user.js
@@ -0,0 +1,29 @@
+const db = require('../config/db');
+const bcrypt = require('bcrypt-nodejs');
+const Promise = require('bluebird');
+const Message = require('./message');
+
+var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+  initialize: function() {
+    this.on('creating', this.hashPassword);
+  },
+  comparePassword: function(attemptedPassword, callback) {
+    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+      callback(isMatch);
+    });
+  },
+  hashPassword: function() {
+    var cipher = Promise.promisify(bcrypt.hash);
+    return cipher(this.get('password'), null, null).bind(this)
+      .then(function(hash) {
+        this.set('password', hash);
+      });
+  },
+  messages: function() {
+    return this.hasMany(Message);
+  }
+});
+
+module.exports = User;
