8df03e4a0d3f03f4f9ea29ee4d43f72d85f6463e
Sam Phillips <armyf35@gmail.com>
2016-11-29 11:42:28 -0500

    Remove guest socket login

--- server/config/socket.js
+++ server/config/socket.js
@@ -4,26 +4,17 @@ module.exports = function(io, activeUsers, guestList, messages) {
       socket.name = name;
       socket.broadcast.emit('login', name);
       activeUsers.push(name);
-      if (name.substr(0, 5) === 'guest') {
-        guestList.push(parseInt(name.substr(5)));
-      }
     });
 
     socket.on('logout', function(name) {
       socket.broadcast.emit('logout', name);
       activeUsers.splice(activeUsers.indexOf(name), 1);
-      if (name.substr(0, 5) === 'guest') {
-        guestList.splice(guestList.indexOf(parseInt(name.substr(5))), 1);
-      }
     });
 
     socket.on('disconnect', function() {
       if (socket.name) {
         socket.broadcast.emit('logout', socket.name);
         activeUsers.splice(activeUsers.indexOf(socket.name), 1);
-        if (socket.name.substr(0, 5) === 'guest') {
-          guestList.splice(guestList.indexOf(parseInt(socket.name.substr(5))), 1);
-        }
       }
     });
 
